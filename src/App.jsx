fb828b26234aa58bde66d5311ccb77114fffada2
Isaac Summers <monogator@gmail.com>
2023-07-14 14:02:04 -0700

    Added some components, test build for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import BookCards from './components/BookCards'
+
+// const url = 'http://localhost:5173/books'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [books, setBooks] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // useEffect(() => {
+  //   async function populateAllBooks() {
+  //     const result = await fetch(url)
+
+  //     setBooks(result)
+  //     console.log(books)
+  //   }
+  //   populateAllBooks()
+  // }, [])
+
+
+  return <>
+    <Header />
+    <BookCards books={books}/>
+  </>
 }
 
 export default App
